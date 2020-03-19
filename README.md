9120beb27a1ffb42223b52362c894872d072f200
dominikbelter <dominik.belter@gmail.com>
2020-03-19 17:15:09 +0100

    instruction added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # stdThreadExamples
 C++ std thread examples
+
+$ cd stdThreadExamples
+
+$ mkdir build
+
+$ cd build
+
+$ cmake ../
+
+$ make
+
+To run the code:
+
+$ cd ../build/bin
+
+$./thread
--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ int main()
 {
     try {
         std::cout << "Thread examples. Run demo programs:\n";
-        std::cout << " - threads_demo - single thread\n";
+        std::cout << "threads_demo - single thread\n";
+        std::cout << "async_demo - asynchronous demo\n";
+        std::cout << "false_sharing_demo - false sharing demo\n";
+        std::cout << "mutex_demo - mutex demo\n";
     }
     catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
